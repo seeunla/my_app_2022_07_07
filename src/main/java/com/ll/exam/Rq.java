2e3ed3002d457d5fcb82f03e3acba2fc8b75614a
seeunla <ksu98712@gmail.com>
2022-07-12 14:21:47 +0900

    Rq 클래스 리팩토링

--- src/main/java/com/ll/exam/Rq.java
+++ src/main/java/com/ll/exam/Rq.java
@@ -5,19 +5,28 @@ import java.util.Arrays;
 
 public class Rq {
     String url;
+    String path;
+    String queryStr;
+    //http://www.naver.com/abc/ddd/fff?age=10&id=40
     public Rq(String url) {
         this.url=url;
+        String[] urlBits = url.split("\\?", 2);
+        this.path = urlBits[0];
+
+        if (urlBits.length ==2) {
+            this.queryStr = urlBits[1];
+        }
     }
 
     public int getIntParam(String paramName, int defaultValue) {
-        String[] urlBits = url.split("\\?", 2);
 
-        if ( urlBits.length==1) {
+
+        if ( queryStr==null) {
             return  defaultValue;
         }
-        urlBits = urlBits[1].split("&");
+        String[] bits = queryStr.split("&");
 
-        for (String urlBit: urlBits) {
+        for (String urlBit: bits) {
             String[] paramNameAndValue = urlBit.split("=",2);
             String paramName_ = paramNameAndValue[0];
             String paramValue = paramNameAndValue[1];
@@ -31,8 +40,6 @@ public class Rq {
     }
 
     public String getPath() {
-        String[] urlBits = url.split("\\?", 2);
-
-        return urlBits[0];
+        return path;
     }
 }
